3ba77c2745a3b3ce36660a42b056360a3e01753a
Nathalia Bustamante <ncbustamanteg@udistrital.edu.co>
2026-02-03 20:39:11 -0500

    v10

--- program.js
+++ program.js
@@ -1,5 +1,6 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([4.681333, -74.040470], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+var marker = L.marker([4.681333, -74.040470]).addTo(map);
